059c2ca3a8362cfd213b30e4ececb002f6806e5f
Jesse-Jackson Zigi <59916143+ZigiJesse-Jackson@users.noreply.github.com>
2022-12-09 14:06:14 +0000

    Adding

---
+++ js/registration_validation.js
@@ -0,0 +1,110 @@
+var phone_regex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g;
+var email_regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+var password_regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm;
+
+
+$(document).ready(function(){
+    $("#first_name").keyup(function(){
+        onInput("#first_name");
+    });
+
+    $("#last_name").keyup(function(){
+        onInput("#last_name");
+    });
+
+    $("#email").keyup(function(){
+        onRegexInput("#email", email_regex);
+    });
+
+    $("#contact").keyup(function(){
+        onRegexInput("#contact", phone_regex);
+    });
+
+    $("#pwd").keyup(function(){
+        onRegexInput("#pwd", password_regex);
+    });
+
+    $("#conf_pwd").keyup(function(){
+        if( validateInput($(this).val()) && $(this).val()==$("#pwd").val()){
+            $(this).css('border-bottom', '1px solid green');
+            return;
+        }else {
+            $(this).css('border-bottom', '1px solid red');
+        }
+    });
+
+    $("#login-button").click(function(e){
+        var f_name = $("#first_name").val().trim();
+        var l_name = $("#last_name").val().trim();
+        var email = $("#email").val().trim();
+        var contact = $("#contact").val().trim();
+        var pwd = $("#pwd").val();
+        var conf_pwd = $("#conf_pwd").val();
+
+        if(validateInput(f_name) && validateInput(l_name) && 
+        validateRegex(email, email_regex) && validateRegex(contact, phone_regex) &&
+        validateRegex(pwd, password_regex) && pwd == conf_pwd){
+            console.log("Sending data");
+            send_data(f_name, l_name, email, contact, pwd, conf_pwd);
+        }
+    });
+
+
+
+
+});
+
+function validateInput(input){
+    if(input == null || input.trim()=="" || input.trim()==null) return false;
+    return true;
+}
+
+function validateRegex(input, regex){
+    if(validateInput(input)==false)return false;
+    if(!input.match(regex))return false;
+    return true;
+
+}
+
+
+function onInput(input){
+    if(validateInput($(input).val())){
+        $(input).css('border-bottom', '1px solid green');
+        return;
+    }else {
+        $(input).css('border-bottom', '1px solid red');
+    }
+}
+
+function onRegexInput(input, regex){
+    if(validateRegex($(input).val(), regex)){
+        $(input).css('border-bottom', '1px solid green');
+        return;
+    }else {
+        $(input).css('border-bottom', '1px solid red');
+    }
+}
+
+function send_data(f_name, l_name, email, contact, password){
+    $.ajax({
+        url: "../login/register_process.php",
+        method: "POST",
+        data: { 
+            register: true,
+            first_name: f_name,
+            last_name: l_name,
+            customer_email: email,
+            contact: contact,
+            password: password
+        },
+        success: function(data){
+            console.log(data);
+            if(data=="0"){
+                
+                //window.location.href = '../login/login.php';
+            }
+        }
+    }); 
+
+}
+
--- login/register_process.php
+++ login/register_process.php
@@ -14,46 +14,27 @@ if (isset($_POST['register'])) {
     $last_name = $_POST['last_name'];
 	$customer_email = $_POST['customer_email'];
 	$password = $_POST['password'];
-    $confirm_password = $_POST['confirm_password'];
 	$contact = $_POST['contact'];
     
-    
-    //check if the two passwords match
-    if ($password != $confirm_password){
-        array_push($errors, "Passwords do not match");
-    }
 
-    //check if email exists
-    $vemail = verify_email($customer_email);
-        
-    if($vemail){
-        echo "Email already exists";
-        }
-
-    //Register user if there are no errors in the form
-    if (count($errors) == 0){
+   
+    //Register user if email has not been used before
+    if (verify_email($customer_email)==false){
         //encrypt password and contact number before adding to database
         $password = password_hash($password, PASSWORD_DEFAULT);
-        
 
         //add new user
-            $addCust = add_customer_function($first_name, $last_name, $customer_email,$contact, $password);
+        $addCust = add_customer_function($first_name, $last_name, $customer_email,$contact, $password);
             
-            if ($addCust){
-                echo "<script type='text/javascript'> alert('Successfully Registered');
-                window.location.href = '../settings/login.php';
-                </script>";
-            }else{
-                echo "Failed";
-            }    
-        }else{ 
-            echo "<script type='text/javascript'> alert('Registration Failed');              
-            window.history.back();
-            </script>";
-        }  
+        if ($addCust){
+            echo "0";
+        }else{
+            echo "1";
+        }    
+    }else{ 
+        echo "2";
+    }  
         
 
 }
-
-
 ?> 
